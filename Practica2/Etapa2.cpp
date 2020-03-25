2c459f4b96fd36950d540ffcae6ffe4038c5d738
andreulopess <andreu.lopez.cortes@gmail.com>
2020-03-25 14:25:49 +0100

    Funciona, falta pintarlo a nes final de es reshape Li he canviat també es colors perque el trobava fei

--- Practica2/Etapa2.cpp
+++ Practica2/Etapa2.cpp
@@ -1,7 +1,5 @@
 // Etapa2.cpp
 // Fichero principal 
-//Viewport: sa part de s'ordenador
-//Finestra : sa part des "mon real" que se representa a sa pantalla
 ////////////////////////////////////////////////////
 
 #include "iostream"
@@ -9,57 +7,38 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-const int W_WIDTH = 500; // Tamaño incial de la ventana
+const int W_WIDTH = 500; // Tamaño incial de la viewport
 const int W_HEIGHT = 500;
 const int wMon = 2;
 const int hMon = 2;
 GLfloat fAngulo; // Variable que indica el ángulo de rotación de los ejes. 
 
-//Función que detecta el cambio de ventana. 
+
 void reshape(int w, int h) {	
-	/*"There are two cases to consider: the aspect of the viewport is 
-	greater than the aspect of your region, so it is wider. In that case,
-	you should map the full height and increase the horitonal range by a 
-	factor of (aspect_viewport/aspect_region). Otherwise, the aspect of the
-	window is lower than aspect of your region, so you should use the full 
-	width and scale up the vertical range by (aspect_region/aspect_viewport). 
-	Note that in both cases, the factor is >= 1."
-	Com que estàs en 2d, assegura't que z1 < z2, si no, no funcionarà i no te dirà el perquè.
-	*/
-	if (h == 0) {
+	
+	if (h == 0) { //per no fer divisions per 0
 		h = 1;
 	}
 	
-	float aspecRatioW = (float)wMon / (float)hMon;
-	float aspectRatioV= (float) w / (float) h;	//aspect des viewport
-	//float aWin = wFin / hFin;	//aspect de sa finestra/mon
-	//awin es 1 per 2/2
-	if (aspectRatioV/aspecRatioW < 1)
-	{
-		printf("Divios a's menor a 1");
-	}
-	
-	if (aspectRatioV > 1) //viewPort major que aspect (aWin) de sa regio  //aspectratio es ATRI
-	{
-		// map the full height and increase the horitonal range by a 
-		//factor of(aspect_viewport / aspect_region)
+	float aspecRatioW = (float)wMon / (float)hMon; //aspect ratio de Window (es 1)
+	float aspectRatioV= (float) w / (float) h;	//aspect ratio des viewport 
+
+	if (aspectRatioV > aspecRatioW) //viewPort major que aspect (aWin) de sa regio  
+	{	//hem de tocar amplada (w) de window. Deixam altura igual i tocam amplada.
+		//Amplada nova = Amplada Anterior * (aViewPort / aWindow)
+		//Posam 0 -+ i /2 perque estigui centrat
 		glLoadIdentity();
-		glOrtho(0 - (aspectRatioV * wMon) / 2, 0 + aspectRatioV *  wMon / 2, -hMon / 2, hMon / 2, -1.0, 1.0f);
-		printf(" ARatio > 1 ");
-		//glOrtho(-(aspectRatio),+(aspectRatio), -hMon/2, hMon/2, -1.0, 1.0f);
-		//glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
+		glOrtho(0 - (wMon* (aspectRatioV/aspecRatioW)  ) / 2, 0 + (wMon * (aspectRatioV / aspecRatioW)) / 2, -hMon / 2, hMon / 2, -1.0, 1.0f);
 	}
 	else
 	{	
-		//so you should use the full
-		//width and scale up the vertical range by(aspect_region / aspect_viewport)
+		//hem de tocar altura (h) de window. Deixam amplada igual i tocam altura.
+		//Altura nova = Amplada Anterior * (aViewPort / aWindow)
+		//Posam 0 -+ perque estigui centrat
 		glLoadIdentity();
-		glOrtho(-wMon/2, wMon/2, 0 - (hMon/2)* aspecRatioW /aspectRatioV, 0+ hMon/2 * aspecRatioW / aspectRatioV, -1.0, 1.0f);
-		printf(" ARatio < 1 ");
-		//glOrtho(-wMon / 2, wMon / 2, -(aspectRatio), +(aspectRatio), -1.0, 1.0f);
-		//glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
+		glOrtho(-wMon/2, wMon/2, 0 - hMon* (aspecRatioW /aspectRatioV) / 2, 0+ hMon * (aspecRatioW / aspectRatioV) / 2, -1.0, 1.0f);
 	}
-	
+	glViewport(0, 0, w, h); //falta tornar a pintar on toca 
 }
 
 // Función que visualiza la escena OpenGL
@@ -79,9 +58,9 @@ void Display(void)
 	glBegin(GL_POLYGON);				
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glVertex3f(0.0f, 0.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex3f(1.0f, 0.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 1.0f);
 	glVertex3f(-0.5f, 0.866f, 0.0f);
 	glEnd();
 
@@ -109,13 +88,13 @@ void Display(void)
 	//Cuadrado. 
 	glBegin(GL_QUADS);
 	glColor3f(1.0, 0.0, 0.0);     
-	glVertex3f(0.5, -0.5, -0.5);      
+	glVertex3f(0.5, -0.5, 0.0);
 	glColor3f(0.0, 1.0, 0.0);     
-	glVertex3f(0.5, 0.5, -0.5);      
+	glVertex3f(0.5, 0.5, 0.0);
 	glColor3f(0.0, 0.0, 1.0);     
-	glVertex3f(-0.5, 0.5, -0.5);     
+	glVertex3f(-0.5, 0.5, 0.0);
 	glColor3f(1.0, 0.0, 1.0);     
-	glVertex3f(-0.5, -0.5, -0.5);
+	glVertex3f(-0.5, -0.5, 0.0);
 
 
 	glEnd();
@@ -132,7 +111,7 @@ void Display(void)
 void Idle(void)
 {
 	// Incrementamos el ángulo
-	fAngulo += 0.3f;
+	fAngulo += 1.0f;
 	// Si es mayor que dos pi la decrementamos
 	if (fAngulo > 360)
 		fAngulo -= 360;
@@ -163,11 +142,9 @@ int main(int argc, char** argv)
 
 	// El color de fondo será el negro (RGBA, RGB + Alpha channel)
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glLoadIdentity();
 	glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
 	//La sintaxi és glOrtho(x min, x max, y min, y max, z1, z2)
-	//glOrtho(left, right, bottom, top, near, far)
-	//osea amplada i altura = 2
+
 
 	// Comienza la ejecución del core de GLUT
 	glutMainLoop();
