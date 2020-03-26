32b39adfb0e16433132cd2cb73da481f2d6ea96c
andreulopess <andreu.lopez.cortes@gmail.com>
2020-03-26 19:11:37 +0100

    Doble Pendulo Fully Working

--- DoblePendulo/DoblePendulo/DoblePendulo.cpp
+++ DoblePendulo/DoblePendulo/DoblePendulo.cpp
@@ -1,19 +1,246 @@
-// DoblePendulo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+#include "iostream"
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
-#include <iostream>
+constexpr auto PI = 3.1415926535897932384626433832795;
+const int W_WIDTH = 500; // Tamaño incial de la viewport
+const int W_HEIGHT = 500;
+const int wMon = 2;
+const int hMon = 2;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+float longitud_corda = 0.3; //longitud corda
+const float x_corda = 0;
+const float y_corda = 0;
+
+const float radiQ = 0.05; //radiquadrat
+
+float angle = PI/4;
+
+float xq, yq;
+float aVel = 0.0;
+float aAcc = 0.0;
+const float m1 = 10;
+const float g = -0.001;
+
+
+
+int draw_endpoint = 0;
+int n_points = 500;
+
+
+//PENDULO 2
+float longitud_corda2 = 0.5;
+float x_corda2;
+float y_corda2;
+float angle2 = PI/4;
+float xq2, yq2;
+float aVel2 = 0.0;
+float aAcc2 = 0.0;
+const float m2 = 10;
+
+
+
+void reshape(int w, int h) {
+
+	if (h == 0) { //per no fer divisions per 0
+		h = 1;
+	}
+
+	float aspecRatioW = (float)wMon / (float)hMon; //aspect ratio de Window (es 1)
+	float aspectRatioV = (float)w / (float)h;	//aspect ratio des viewport 
+
+	if (aspectRatioV > aspecRatioW) //viewPort major que aspect (aWin) de sa regio  
+	{	//hem de tocar amplada (w) de window. Deixam altura igual i tocam amplada.
+		//Amplada nova = Amplada Anterior * (aViewPort / aWindow)
+		//Posam 0 -+ i /2 perque estigui centrat
+		glLoadIdentity();
+		glOrtho(0 - (wMon * (aspectRatioV / aspecRatioW)) / 2, 0 + (wMon * (aspectRatioV / aspecRatioW)) / 2, -hMon / 2, hMon / 2, -1.0, 1.0f);
+	}
+	else
+	{
+		//hem de tocar altura (h) de window. Deixam amplada igual i tocam altura.
+		//Altura nova = Amplada Anterior * (aViewPort / aWindow)
+		//Posam 0 -+ perque estigui centrat
+		glLoadIdentity();
+		glOrtho(-wMon / 2, wMon / 2, 0 - hMon * (aspecRatioW / aspectRatioV) / 2, 0 + hMon * (aspecRatioW / aspectRatioV) / 2, -1.0, 1.0f);
+	}
+	glViewport(0, 0, w, h); //pintam segons es canvi des viewport
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+void pintarQuadrat() {
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	
+	//float yquadrat = y-longitud_corda;
+	float xant = xq;
+	float yant = yq;
+	//actualitzam els punts del quadrat
+	xq = x_corda + longitud_corda * sin(angle);
+	yq = y_corda + longitud_corda * cos(angle);
+
+	xq2 = xq + longitud_corda2 * sin(angle2);
+	yq2 = yq + longitud_corda2 * cos(angle2);
+
+
+	/*xq2 = xq + longitud_corda2 * sin(angle2);
+	yq2 = yq + longitud_corda2 * cos(angle2);
+	*/
+
+
+	//Pintam quadrat pendulo 1
+	glBegin(GL_QUADS);
+	glColor3f(0.0, 0.0, 0.0);
+	glVertex3f(xq + radiQ, yq - radiQ, 0.0);
+	//glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(xq + radiQ, yq + radiQ, 0.0);
+	//glColor3f(1.0, 1.0, 0.0);
+	glVertex3f(xq - radiQ, yq + radiQ, 0.0);
+	//glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(xq - radiQ, yq - radiQ, 0.0);
+	glEnd();
+
+	
+	//Pintam quadrat pendulo 2
+	glBegin(GL_QUADS);
+	glColor3f(0.0, 0.0, 0.0);
+	glVertex3f(xq2 + radiQ, yq2 - radiQ, 0.0);
+	//glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(xq2 + radiQ, yq2 + radiQ, 0.0);
+	//glColor3f(1.0, 1.0, 0.0);
+	glVertex3f(xq2 - radiQ, yq2 + radiQ, 0.0);
+	//glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(xq2 - radiQ, yq2 - radiQ, 0.0);
+	glEnd();
+
+	
+
+}
+// Función que visualiza la escena OpenGL
+
+void calcular() {
+
+
+
+	float num1 = -g * (2 * m1 + m2) * sin(angle);
+	float num2 = -m2 * g * sin(angle - 2 * angle2);
+	float num3 = -2 * sin(angle - angle2) * m2;
+	float num4 = aVel2 * aVel2 * longitud_corda2 + aVel * aVel * longitud_corda * cos(angle - angle2);
+	float den = longitud_corda * (2 * m1 + m2 - m2 * cos(2 * angle - 2 * angle2));
+	 aAcc = (num1 + num2 + num3 * num4) / den;
+
+
+	num1 = 2 * sin(angle - angle2);
+	num2 = (aVel * aVel * longitud_corda * (m1 + m2));
+	num3 = g * (m1 + m2) * cos(angle);
+	num4 = aVel2* aVel2 * longitud_corda2 * m2 * cos(angle - angle2);
+	den = longitud_corda2 * (2 * m1 + m2 - m2 * cos(2 * angle - 2 * angle2));
+	aAcc2 = (num1 * (num2 + num3 + num4)) / den;
+
+
+	
+}
+void Display(void)
+{
+	//Se llama a la función que tiene que mantener la relación de aspecto pasando por parámetro el tamaño de la ventana. 
+	glutReshapeFunc(reshape);
+
+
+	// Borramos la escena
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+
+	//Se hace un escalado reduciendo el tamaño un 70% para el dibujado del cuadrado. 
+	//glScalef(0.2f, 0.2f, 0.2f);
+
+	/*glBegin(GL_LINES); // pintar sostre
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glEnd();*/
+
+
+	
+	pintarQuadrat();
+
+	glBegin(GL_LINES); // pintar corda
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(x_corda, y_corda, 0.0f);
+	glVertex3f(xq, yq, 0.0f);
+
+	
+	glVertex3f(xq, yq, 0.0f);
+	glVertex3f(xq2, yq2, 0.0f);
+	
+
+	glEnd();
+
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
+
+
+
+	glPopMatrix();
+
+	glFlush();
+
+	//Cambio entre el backbuffer y el frontbuffer.
+	glutSwapBuffers();
+}
+
+// Función que se ejecuta cuando el sistema no esta ocupado
+void Idle(void)
+{
+
+	//per pintar punts
+	calcular();
+
+	//glutPostRedisplay();
+	
+	// Incrementamos el ángulo
+	aVel += aAcc;
+	aVel2 += aAcc2;
+
+	angle += aVel;
+	angle2 += aVel2;
+
+	//Si descomentes aixo se va frenant
+	//aVel *= 0.99;
+	//aVel2 *= 0.99;
+	
+
+	// Indicamos que es necesario repintar la pantalla
+	glutPostRedisplay();
+}
+
+
+// Función principal
+int main(int argc, char** argv)
+{
+	// Inicializamos la librería GLUT
+	glutInit(&argc, argv);
+
+	// Indicamos como ha de ser la nueva ventana
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
+
+	//Se habilita el doblebuffer con el comando "GLUT_DOUBLE". 
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+
+	// Creamos la nueva ventana
+	glutCreateWindow("Etapa 2 - Péndulo");
+
+	// Indicamos cuales son las funciones de redibujado e idle
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
+
+	// El color de fondo será el negro (RGBA, RGB + Alpha channel)
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	//glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
+	//La sintaxi és glOrtho(x min, x max, y min, y max, z1, z2)
+
+
+	// Comienza la ejecución del core de GLUT
+	glutMainLoop();
+	return 0;
+}
