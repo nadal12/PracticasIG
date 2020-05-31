f9442365933de4925b8a639aec7f19ae7ebcaadb
Nadal Llabrés <nadal12@hotmail.com>
2020-05-31 16:56:27 +0200

    Creación del proyecto etapa 6.

---
+++ Practica6/Practica6.cpp
@@ -0,0 +1,166 @@
+// Etapa2.cpp
+// Fichero principal 
+////////////////////////////////////////////////////
+
+#include "iostream"
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+const int W_WIDTH = 500; // Tamaño incial de la viewport
+const int W_HEIGHT = 500;
+const int wMon = 2;
+const int hMon = 2;
+GLfloat fAngulo; // Variable que indica el ángulo de rotación de los ejes. 
+
+
+void reshape(int w, int h) {
+
+	if (h == 0) { //per no fer divisions per 0
+		h = 1;
+	}
+
+	float aspecRatioW = (float)wMon / (float)hMon; //aspect ratio del Window
+	float aspectRatioV = (float)w / (float)h;	//aspect ratio del viewport 
+
+	if (aspectRatioV > aspecRatioW)
+		//ViewPort major que aspect (aWin) de la regió  
+	{
+		//Amplada nova = Amplada anterior * (aViewPort / aWindow)
+		//0 -+ i /2 para centrarlo
+		glLoadIdentity();
+		glOrtho((GLdouble)0 - (wMon * (aspectRatioV / aspecRatioW)) / 2, (GLdouble)0 + (wMon * (aspectRatioV / aspecRatioW)) / 2, (GLdouble)-hMon / 2, (GLdouble)hMon / 2, (GLdouble)-1.0, (GLdouble)1.0f);
+	}
+	else
+	{
+
+		//Altura nova = Amplada Anterior * (aViewPort / aWindow)
+		//Posam 0 -+ perque estigui centrat
+		glLoadIdentity();
+		glOrtho((GLdouble)-wMon / 2, wMon / 2, (GLdouble)0 - hMon * (aspecRatioW / aspectRatioV) / 2, (GLdouble)0 + hMon * (aspecRatioW / aspectRatioV) / 2, (GLdouble)-1.0, (GLdouble)1.0f);
+	}
+	glViewport(0, 0, w, h); //Pintam segons es canvi des viewport
+}
+
+// Función que visualiza la escena OpenGL
+void Display(void)
+{
+	// Borramos la escena
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+
+	//Pintar ejes de coordenadas
+	glBegin(GL_LINES);     // pintar los ejes X
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glEnd();
+
+	glBegin(GL_LINES); // pintar los ejes Y
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glEnd();
+
+	// Rotamos las proximas primitivas
+	glRotatef(fAngulo, 0.0f, 0.0f, 1.0f);
+
+
+	// Creamos a continuación dibujamos los tres poligonos
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glEnd();
+
+	//Se hace un escalado reduciendo el tamaño un 70% para el dibujado del cuadrado. 
+	glScalef(0.3f, 0.3f, 0.3f);
+
+	//Cuadrado. 
+	glBegin(GL_QUADS);
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+	glColor3f(1.0, 0.0, 1.0);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glEnd();
+
+	glPopMatrix();
+
+	//Cambio entre el backbuffer y el frontbuffer.
+
+	glutSwapBuffers();
+}
+
+// Función que se ejecuta cuando el sistema no esta ocupado
+void Idle(void)
+{
+	// Incrementamos el ángulo
+	fAngulo += 1.0f;
+	// Si es mayor que dos pi la decrementamos
+	if (fAngulo > 360)
+		fAngulo -= 360;
+	// Indicamos que es necesario repintar la pantalla
+	glutPostRedisplay();
+}
+
+
+// Función principal
+int main(int argc, char** argv)
+{
+	// Inicializamos la librería GLUT
+	glutInit(&argc, argv);
+
+	// Indicamos como ha de ser la nueva ventana
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
+
+	//Se habilita el doblebuffer con el comando "GLUT_DOUBLE". 
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+
+	// Creamos la nueva ventana
+	glutCreateWindow("ViewPort - Etapa 2");
+
+
+	// Indicamos cuales son las funciones de redibujado e idle
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
+
+	//Se indica cual es la función de reshape.
+	glutReshapeFunc(reshape);
+
+	// El color de fondo será el negro (RGBA, RGB + Alpha channel)
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
+	//La sintaxi és glOrtho(x min, x max, y min, y max, z1, z2)
+
+
+	// Comienza la ejecución del core de GLUT
+	glutMainLoop();
+	return 0;
+}
