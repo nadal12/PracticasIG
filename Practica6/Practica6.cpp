0480dbaf7a07e7718988fbd3b2b96f8931bbdb5c
andreulopess <andreu.lopez.cortes@gmail.com>
2020-06-04 17:18:35 +0200

    Minimapa implementat

--- Practica6/Practica6.cpp
+++ Practica6/Practica6.cpp
@@ -8,25 +8,18 @@
 #include <GL/glut.h>
 #endif
 
-#include <vector>
-
-//////////////////////////////////////////////////////////////////
-//                               DEFINICI? DE VARIABLES         //
-#define BASE_HEIGHT  0.25
-#define BASE_WIDE    0.05
-
 // angle of rotation for the camera direction
-GLdouble angle = 0.0f;
-GLfloat fAnguloFig2;
+float angle = 0.0f;
 
 // actual vector representing the camera's direction
-GLdouble lx = 0.0f, lz = -1.0f, ly = 0.0f;
+float lx = 0.0f, lz = -1.0f, ly = 0.0f;
 
 // XZ position of the camera
-GLdouble x = 0.0f, z = 5.0f, y = 1.5f;
+float x = 0.0f, z = 5.0f, y = 1.75f;
+bool freeview = false;
 
 // the key states. These variables will be zero
-// when no key is being presses
+//when no key is being presses
 float deltaAngle = 0.0f;
 float deltaMove = 0;
 int xOrigin = -1;
@@ -36,49 +29,13 @@ int h, w;
 
 // variables to compute frames per second
 int frame;
-GLdouble time, timebase;
+long time, timebase;
 char s[50];
 
 // variables to hold window identifiers
-int mainWindow, subWindow1, subWindow2, subWindow3;
+int mainWindow, subWindow1, subWindow2;
 //border between subwindows
-int border = 5;
-//nombre per decidir quina figura pintam
-int figura = 1;
-
-bool forapitjar = false;
-int prespect = 1;
-bool freeview = false;
-//Lorenz Atracctor
-float xlorenz = 0.01;
-float ylorenz = 0;
-float zlorenz = 0;
-
-float dx;
-float dy;
-float dz;
-
-float dt = 0.01;
-
-float a = 10;
-float b = 5;
-float c = 8.0 / 3.0;
-struct Point
-{
-	float x, y, z;
-	//unsigned char r, g, b, a;
-};
-std::vector< Point > points;
-
-//Trajectoria
-const int trajectoryLenght = 1000;
-float trajectoryX[trajectoryLenght];
-float trajectoryY[trajectoryLenght];
-float trajectoryZ[trajectoryLenght];
-
-int indexQ2 = 0;
-//==============================================================//
-
+int border = 6;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -92,9 +49,7 @@ int yOrigin = -1;
 
 
 
-//////////////////////////////////////////////////////
-//                                RESHAPE           //
-// Aquesta funci? l'usarem pes reshape des viewport //
+
 void setProjection(int w1, int h1)
 {
 	float ratio;
@@ -102,17 +57,17 @@ void setProjection(int w1, int h1)
 	// (you cant make a window of zero width).
 	ratio = 1.0f * w1 / h1;
 	// Reset the coordinate system before modifying
-	glMatrixMode(GL_PROJECTION); //ProjectionView is the matrix that represents your camera's lens (aperture, far-field, near-field, etc).
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
 	// Set the viewport to be the entire window
 	glViewport(0, 0, w1, h1);
 
 	// Set the clipping volume
-	gluPerspective(45, ratio, 0.1, 1000); //Referent a s'entorn, com el veus
-	glMatrixMode(GL_MODELVIEW); //ModelView is the matrix that represents your camera (position, pointing, and up vector).
+	gluPerspective(45, ratio, 0.1, 1000);
+	glMatrixMode(GL_MODELVIEW);
 }
-//Lo que a ses anteriors etapes li deiem reshape :D //
+
 void changeSize(int w1, int h1) {
 
 	if (h1 == 0)
@@ -122,14 +77,25 @@ void changeSize(int w1, int h1) {
 	w = w1;
 	h = h1;
 
-	// set subwindow 1 as the active window
-	// resize and reposition the sub window
-	//glutPositionWindow(border, border);
-	glutReshapeWindow(w, h);
-	setProjection(w, h);
-}
-//==================================================//
 
+	// set subwindow 2 as the active window
+	glutSetWindow(subWindow2);
+	// resize and reposition the sub window
+	glutPositionWindow((w + border) *0.75, border);
+	glutReshapeWindow(w / 4 - border * 3 / 2, h / 4 - border * 3 / 2);
+	setProjection(w / 4 - border * 3 / 2, h / 4 - border * 3 / 2); 
+	
+	
+	// set subwindow 1 as the active window
+	glutSetWindow(subWindow1);
+	// resize and reposition the sub window
+	glutPositionWindow(border, border);
+	glutReshapeWindow(w,h);
+	setProjection(w,h);
+
+	
+	
+}
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -198,24 +164,32 @@ void draw_streetlight() {
 //==========================================================================//
 
 
-/*
-GLfloat decrementarAngulo(GLfloat angulo) {
-	if (angulo > 360) {
-		angulo -= 360;
+void renderBitmapString(
+	float x,
+	float y,
+	float z,
+	void* font,
+	char* string) {
+
+	char* c;
+	glRasterPos3f(x, y, z);
+	for (c = string; *c != '\0'; c++) {
+		glutBitmapCharacter(font, *c);
 	}
-	return angulo;
 }
-*/
 
 void restorePerspectiveProjection() {
+
 	glMatrixMode(GL_PROJECTION);
 	// restore previous projection matrix
 	glPopMatrix();
+
 	// get back to modelview mode
 	glMatrixMode(GL_MODELVIEW);
 }
 
 void setOrthographicProjection() {
+
 	// switch to projection mode
 	glMatrixMode(GL_PROJECTION);
 
@@ -246,11 +220,9 @@ void computePos(float deltaMove) {
 	{
 		y = 1.5;
 	}
-
 }
-
-// Impresió objectes en l'escena
-void render_items() {
+// Common Render Items for all subwindows
+void renderScene2() {
 
 	// Terra
 	glColor3f(0.0f, 0.6f, 0.0f);
@@ -265,36 +237,43 @@ void render_items() {
 	draw_streetlight();
 }
 
-
-
-// Display func
+// Display func for main window
 void renderScene() {
-	// check for keyboard movement
-	if (!forapitjar)
-	{
-		if (deltaMove) {
-			computePos(deltaMove);
-			glutPostRedisplay();
-		}
-	}
+	glutSetWindow(mainWindow);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
 
+// Display func for sub window 1
+void renderScenesw1() {
+
+	glutSetWindow(subWindow1);
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
-
 	gluLookAt(x, y, z,
 		x + lx, y + ly, z + lz,
-		0, 1, 0);
+		0.0f, 1.0f, 0.0f);
 
-	// Cridam la funció que imprimeix tots els objectes
-	render_items();
+	renderScene2();
+
+	// display fps in the top window
+	frame++;
+
+	time = glutGet(GLUT_ELAPSED_TIME);
+	if (time - timebase > 1000) {
+		sprintf_s(s, "Lighthouse3D - FPS:%4.2f",
+			frame * 1000.0 / (time - timebase));
+		timebase = time;
+		frame = 0;
+	}
 
 	setOrthographicProjection();
 
 	glPushMatrix();
 	glLoadIdentity();
-
+	renderBitmapString(5, 30, 0, GLUT_BITMAP_HELVETICA_12, s);
 	glPopMatrix();
 
 	restorePerspectiveProjection();
@@ -302,18 +281,82 @@ void renderScene() {
 	glutSwapBuffers();
 }
 
+// Display func for sub window 2
+void renderScenesw2() {
 
+	glutSetWindow(subWindow2);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	gluLookAt(x, y + 30, z,
+		x, y - 1, z,
+		lx, 0, lz);
+
+	// Draw red cone at the location of the main camera
+	glPushMatrix();
+	glColor3f(1.0, 0.0, 0.0);
+	glTranslatef(x, y, z);
+	glRotatef(180 - (angle + deltaAngle) * 180.0 / 3.14, 0.0, 1.0, 0.0);
+	glutSolidCone(0.2, 0.8f, 4, 4);
+	glPopMatrix();
+
+	renderScene2();
+
+	glutSwapBuffers();
+}
+
+// Display func for sub window 3
+/*void renderScenesw3() {
+
+	glutSetWindow(subWindow3);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	gluLookAt(x - lz * 10, y, z + lx * 10,
+		x, y, z,
+		0.0f, 1.0f, 0.0f);
+
+	// Draw red cone at the location of the main camera
+	glPushMatrix();
+	glColor3f(1.0, 0.0, 0.0);
+	glTranslatef(x, y, z);
+	glRotatef(180 - (angle + deltaAngle) * 180.0 / 3.14, 0.0, 1.0, 0.0);
+	glutSolidCone(0.2, 0.8f, 4, 4);
+	glPopMatrix();
+
+	renderScene2();
+
+	glutSwapBuffers();
+}*/
+
+// Global render func
+void renderSceneAll() {
+
+	// check for keyboard movement
+	if (deltaMove) {
+		computePos(deltaMove);
+		glutSetWindow(mainWindow);
+		glutPostRedisplay();
+	}
+
+	renderScene();
+	renderScenesw1();
+	renderScenesw2();
+	//renderScenesw3();
+}
 
 // -----------------------------------
 //             KEYBOARD
 // -----------------------------------
+
 void processNormalKeys(unsigned char key, int xx, int yy) {
 
-	if (key == 27) // si apretes ESC te tanca sa finestra
-	{
+	if (key == 27) {
+		glutDestroyWindow(mainWindow);
 		exit(0);
 	}
-
 	if (key == ' ') {
 		//allibera eix y
 		freeview = !freeview;
@@ -324,12 +367,12 @@ void pressKey(int key, int xx, int yy) {
 
 	switch (key) {
 	case GLUT_KEY_UP: deltaMove = 0.5f; break;
-		//case GLUT_KEY_RIGHT: deltaMove = 0.5f; break;
 	case GLUT_KEY_DOWN: deltaMove = -0.5f; break;
 	}
+	glutSetWindow(mainWindow);
 	glutPostRedisplay();
-}
 
+}
 
 void releaseKey(int key, int x, int y) {
 
@@ -338,7 +381,6 @@ void releaseKey(int key, int x, int y) {
 	case GLUT_KEY_DOWN: deltaMove = 0; break;
 	}
 }
-
 
 // -----------------------------------
 //             MOUSE
@@ -357,7 +399,7 @@ void mouseMove(int x, int y) {
 		lx = sin(angle + deltaAngle) * sin(beta + betaAngle);
 		lz = -cos(angle + deltaAngle) * sin(beta + betaAngle);
 		ly = cos(beta + betaAngle);
-
+		glutSetWindow(mainWindow);
 		glutPostRedisplay();
 	}
 }
@@ -365,29 +407,25 @@ void mouseMove(int x, int y) {
 void mouseButton(int button, int state, int x, int y) {
 
 	// only start motion if the left button is pressed
-	if (!forapitjar) {
-		if (button == GLUT_LEFT) {
+	if (button == GLUT_LEFT) {
 
-			// when the button is released
-			if (state == GLUT_UP) {
-				angle += deltaAngle;
-				beta += betaAngle;
-				deltaAngle = 0.0f;
-				betaAngle = 0.0f;
-				xOrigin = -1;
-				yOrigin = -1;
+		// when the button is released
+		if (state == GLUT_UP) {
+			angle += deltaAngle;
+			beta += betaAngle;
+			deltaAngle = 0.0f;
+			betaAngle = 0.0f;
+			xOrigin = -1;
+			yOrigin = -1;
 
-			}
-			else {// state = GLUT_DOWN
+		}
+		else {// state = GLUT_DOWN
 
-				xOrigin = x;
-				yOrigin = y;
-			}
+			xOrigin = x;
+			yOrigin = y;
 		}
 	}
 }
-
-
 
 // -----------------------------------
 //             MAIN and INIT
@@ -409,22 +447,37 @@ void init() {
 }
 
 int main(int argc, char** argv) {
+
 	// init GLUT and create main window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 800);
-	glutCreateWindow("Etapa6");
+	mainWindow = glutCreateWindow("Etapa 6 Informática Gráfica");
 
 	// callbacks for main window
-	glutDisplayFunc(render_items);
+	glutDisplayFunc(renderSceneAll);
 	glutReshapeFunc(changeSize);
 
 	// Removing the idle function to save CPU and GPU
-	glutIdleFunc(renderScene);
-
+	//glutIdleFunc(renderSceneAll);
 	init();
 
+	subWindow2 = glutCreateSubWindow(mainWindow, (w + border) * 0.75, border, w / 4 - border * 3 / 2, h / 4 - border * 3 / 2);
+	glutDisplayFunc(renderScenesw2);
+	init();
+
+	// sub windows
+	subWindow1 = glutCreateSubWindow(mainWindow, border, border, w - 2 * border, h / 2 - border * 3 / 2);
+	glutDisplayFunc(renderScenesw1);
+	init();
+
+
+
+	/*subWindow3 = glutCreateSubWindow(mainWindow, (w + border) / 2, (h + border) / 2, w / 2 - border * 3 / 2, h / 2 - border * 3 / 2);
+	glutDisplayFunc(renderScenesw3);
+	init();
+	*/
 	// enter GLUT event processing cycle
 	glutMainLoop();
 
